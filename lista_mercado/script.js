7ce4b548b55dd41bf3b2fbbf1e805d00d7e6fb8a
PietraKruger <Pietrapikruger11@gmail.com>
2026-06-14 18:55:01 -0300

    Controle de Fluxo: Laços de Repetição

---
+++ lista_mercado/script.js
@@ -0,0 +1,22 @@
+// const turma1 = ["Fábio", "Cristina", "Jéssica"];
+// const turma2 = ["Pietra", "Pedro"];
+
+// console.log("Alunos da turma 1:",turma1);
+// console.log("Alunos da turma 2:",turma2);
+
+// console.log(turma2[0]);
+
+const listaDeCompras = ["arroz", "feijão", "ovos"];
+listaDeCompras.push("Morango");
+console.log(listaDeCompras.length);
+console.log (listaDeCompras[listaDeCompras.length - 1]);
+listaDeCompras[listaDeCompras.length] = "Banana";
+console.log(listaDeCompras)
+
+// listaDeCompras[3] = "Farofa";
+
+// console.log(listaDeCompras);
+
+// listaDeCompras.push("Maçã")
+
+// console.log(listaDeCompras)
--- script.js
+++ script.js
@@ -1,22 +1,85 @@
-// const turma1 = ["Fábio", "Cristina", "Jéssica"];
-// const turma2 = ["Pietra", "Pedro"];
+const carros = ["uno", "fusca", "escort", "gol"];
+const conteudo = document.getElementById("conteudo");
 
-// console.log("Alunos da turma 1:",turma1);
-// console.log("Alunos da turma 2:",turma2);
+var dados;
+var i;
 
-// console.log(turma2[0]);
+function criaSecao(titulo,dadosf){
+    let secao = document.createElement("div");
+secao.innerHTML = "<h2>"+titulo+"</h2>"+dadosf;
+conteudo.appendChild(secao);
+}
 
-const listaDeCompras = ["arroz", "feijão", "ovos"];
-listaDeCompras.push("Morango");
-console.log(listaDeCompras.length);
-console.log (listaDeCompras[listaDeCompras.length - 1]);
-listaDeCompras[listaDeCompras.length] = "Banana";
-console.log(listaDeCompras)
+dados = "";
+i = 0;
 
-// listaDeCompras[3] = "Farofa";
+while (i < 4){
+dados += "<p>" +carros[i]+ "</p>";
+i++;
+};
+criaSecao("Loop while",dados);
 
-// console.log(listaDeCompras);
+dados = "";
+i = 0;
 
-// listaDeCompras.push("Maçã")
+while (i < carros.length){
+dados += "<p>" +carros[i]+ "</p>";
+i++;
+};
+criaSecao("Loop while melhorado",dados);
 
-// console.log(listaDeCompras)
+dados = "";
+i = 0;
+
+do {
+    dados += "<p>" +carros[i]+ "</p>";
+i++;
+} while (i < carros.length);
+criaSecao("Loop while invertido",dados);
+
+
+dados = "";
+
+for (i = 0; i < carros.length; i++) {
+    dados += "<p>" +carros[i]+ "</p>";
+}
+criaSecao("Loop for",dados);
+
+
+dados = "";
+
+for (let carro of carros) {
+    if(carro === "escort")
+    break;
+    dados += "<p>" +carro+ "</p>";
+}
+criaSecao("Loop for of",dados);
+
+
+let carro1 = {marca: "ford", modelo: "ka", ano: "2015"};
+let carro2 = {marca: "fiat", modelo: "uno", ano: "2000"};
+let carros2 = [];
+carros2.push(carro1);
+carros2.push(carro2);
+dados = "";
+
+for (let carro of carros2) {
+let propriedades = "";
+
+    for (let prop in carro) {
+        
+        propriedades += carro[prop] + " | ";
+        
+    }
+
+    dados += "<p>" +propriedades+ "</p>";
+}
+criaSecao("Loop for in",dados);
+
+
+dados = "";
+
+carros.forEach((carro) => {
+    dados += "<p>" +carro+ "</p>";
+});
+criaSecao("Loop for each",dados);
