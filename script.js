9caaaf2ff82a9e32cf149d5f8fb183ff11b94872
PietraKruger <Pietrapikruger11@gmail.com>
2026-06-15 02:19:14 -0300

    Validação de Formulários com JS

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-const celulas = document.querySelectorAll(".celula");
-let vezDoX = true;
-document.getElementById("reiniciar").addEventListener("click",iniciarJogo);
+function validateForm(){
 
-function iniciarJogo(){
-    celulas.forEach(celula =>{
-        celula.textContent = "";
-        celula.addEventListener("click", tratarClick, {once:true});
-    })
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const message = document.getElementById("message").value;
+
+if (name === '' || email === '' || message === '') {
+    alert("Todos os campos devem ser preenchidos");
+    return false;
 }
 
-function tratarClick(evento){
-    evento.target.textContent = vezDoX ? "X" : "O";
-    vezDoX = !vezDoX;
+if (name.length < 3 || name.length > 50) {
+    alert("O nome deve ter entre 3 e 50 caracteres");
+    return false;
 }
 
-iniciarJogo();
+if (email.length < 5 || email.length > 50) {
+    alert("O email deve ter entre 5 e 50 caracteres");
+    return false;
+}
+
+const emailPatern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if (!emailPatern.test(email)) {
+    alert("Por favor, insira um email valido");
+    return false;
+}
+
+return true;
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    if (validateForm()) {
+        alert("Formulario validado!");
+    }
+})
