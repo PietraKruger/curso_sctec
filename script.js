19dab3129f1729948982a10493fa7c7785b13720
PietraKruger <Pietrapikruger11@gmail.com>
2026-06-17 00:59:23 -0300

    POO: Programação Orientada a Objetos (Parte 1)

---
+++ script.js
@@ -0,0 +1,12 @@
+class Carro {
+    constructor(frabricante,marca,ano,tipo,qtdPortas){
+        this.frabricante = marca;
+        this.marca = marca;
+        this.ano = marca;
+        this.tipo = marca;
+        this.qtdPortas = marca; //Propriedade da Classe
+    }
+    mostrarMarca(){
+        console.log(`A marca do carro é ${this.marca}`);
+    }
+}
