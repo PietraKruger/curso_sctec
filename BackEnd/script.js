544be47b6ed0fe8215cb3ac5b262bb7bfe3dce1d
PietraKruger <Pietrapikruger11@gmail.com>
2026-06-23 00:15:49 -0300

    POO: Programação Orientada a Objetos (Parte 2)

--- BackEnd/script.js
+++ BackEnd/script.js
@@ -1,12 +1,41 @@
-class Carro {
-    constructor(frabricante,marca,ano,tipo,qtdPortas){
-        this.frabricante = marca;
-        this.marca = marca;
-        this.ano = marca;
-        this.tipo = marca;
-        this.qtdPortas = marca; //Propriedade da Classe
-    }
-    mostrarMarca(){
-        console.log(`A marca do carro é ${this.marca}`);
-    }
+class Veiculo {
+  constructor(fabricante, modelo, ano, tipo, qtdPortas) {
+    this.frabricante = fabricante;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.tipo = tipo;
+    this.qtdPortas = qtdPortas; //Propriedade da Classe
+  }
+
+  mostrarDadosDoVeiculo() {
+    console.log(
+      `${this.frabricante} ${this.modelo}, ${this.ano} (${this.tipo}), ${this.qtdPortas} portas`,
+    );
+  }
+
+  mostrarModelo() {
+    console.log(`O modelo do carro é: ${this.modelo}`);
+  }
 }
+class Carro extends Veiculo {
+  constructor(fabricante, modelo, ano, tipo, qtdPortas) {
+    super(fabricante, modelo, ano, tipo, qtdPortas);
+  }
+}
+
+class Moto extends Veiculo {
+  constructor(fabricante, modelo, ano, cilidradas) {
+    super(fabricante, modelo, ano);
+    this.cilidradas = cilidradas;
+  }
+
+  mostrarDadosDoVeiculo() {
+    console.log(`${this.frabricante} ${this.modelo}, ${this.ano}, ${this.cilidradas} cilidradas`);
+  }
+}
+
+const meuCarro = new Carro("Ford", "KA", "2000", "Sedan", 4);
+meuCarro.mostrarDadosDoVeiculo();
+
+const minhaMoto = new Moto("Yamaha", "K2", "2000", "300");
+minhaMoto.mostrarDadosDoVeiculo();
